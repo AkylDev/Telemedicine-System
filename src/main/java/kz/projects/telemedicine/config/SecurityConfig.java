7ec27db74121165a37454fcdb1397cda82042519
Akello-web <aklbktglv@gmail.com>
2024-07-10 00:57:30 +0500

    little fixing

--- src/main/java/kz/projects/telemedicine/config/SecurityConfig.java
+++ src/main/java/kz/projects/telemedicine/config/SecurityConfig.java
@@ -12,11 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -48,27 +46,11 @@ public class SecurityConfig {
             .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
                     .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
             .formLogin(AbstractAuthenticationFilterConfigurer::disable)
-            .logout(httpSecurityLogoutConfigurer -> {
-              httpSecurityLogoutConfigurer
-                      .logoutUrl("/auth/logout")
-                      .permitAll();
-            });
+            .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
+                    .logoutUrl("/auth/logout")
+                    .permitAll());
 
     return http.build();
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    manager.createUser(User.withUsername("user")
-            .password(passwordEncoder.encode("userPass"))
-            .roles("USER")
-            .build());
-    manager.createUser(User.withUsername("admin")
-            .password(passwordEncoder.encode("adminPass"))
-            .roles("USER", "ADMIN")
-            .build());
-    return manager;
   }
 
   @Bean
